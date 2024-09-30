38a9ab12ef50b3cca4d3f73a39259838fc3509c4
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-09-30 18:40:12 +0200

    stavolta la batteria è finita

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 let navbar = document.querySelector('.navbar');
+let links = document.querySelectorAll('.nav-link');
+let logoNavbar = document.querySelector('#logoNavbar');
+let logoWidth = document.querySelectorAll('.logo');
+
+console.log(logoWidth);
+
+
+
 
 window.addEventListener('scroll', ()=>{
-    console.log(window.scrollY);
+    let scrolled = window.scrollY;
+
+    if (scrolled > 0) {
+        navbar.classList.remove('bg-gunmetal');
+        navbar.classList.add('bg-ashGray');
+        navbar.style.height= '70px';
+        links.forEach( (link)=> {
+            link.style.color= 'var(--gunmetal)';
+        })
+        logoNavbar.src= 'http://127.0.0.1:5500/Media/logo%20gunMetal.png';
+        logoWidth.forEach( (dir) =>{
+            dir.classList.remove('logo');
+            dir.classList.add('logoSmall');
+        })
+        
+    }else{
+        navbar.classList.remove('bg-ashGray');
+        navbar.classList.add('bg-gunmetal');
+        navbar.style.height= '100px';
+        links.forEach( (link)=> {
+            link.style.color= 'var(--jasper)';
+        })
+        logoNavbar.src= 'http://127.0.0.1:5500/Media/logo%20jasper.png'
+        logoWidth.forEach( (dir) =>{
+            dir.classList.remove('logoSmall');
+            dir.classList.add('logo');
+        })
+    }
     
 });
