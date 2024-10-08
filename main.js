42101bbd9da4a0db00e2232957904734db65c4ac
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-10-08 17:16:26 +0200

    finita la sezione commenti con swiper

--- main.js
+++ main.js
@@ -89,17 +89,17 @@ function createInterval(n, element, time) {
         }
         
     }, time);
-
+    
     setTimeout(() => {
         confirm = true;
     }, 5000);
-
+    
 }
 
 
 
 
-let observer = new IntersectionObserver( (entries)=>{
+let starterPoint = new IntersectionObserver( (entries)=>{
     entries.forEach( (entry)=> {
         if (entry.isIntersecting && confirm) {
             createInterval(100, firstNumber, 10);
@@ -110,7 +110,98 @@ let observer = new IntersectionObserver( (entries)=>{
     });
 } );
 
-observer.observe(firstNumber);
+starterPoint.observe(firstNumber);
+
+
+
+
+
+// Recensioni e Swiper
+
+let reviews = [
+    {user: 'Cicala84', description: `Sempre fantastici!!`, rank: 5},
+    {user: 'Raziel82', description: `Li adoro!!`, rank: 5},
+    {user: 'Antonio', description: `Esistono ancora metallari?`, rank: 1},
+    {user: 'Filippo', description: `Boomer`, rank: 1},
+    {user: 'Manuela', description: `I due commenti che precedono il mio non sono attendibili!`, rank: 5},
+]
+
+let swiperWrapper = document.querySelector('.swiper-wrapper');
+
+reviews.forEach( (recensione)=>{
+    let div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+        <div class="card-review">
+            <p class="lead2 text-center">${recensione.description}</p>
+            <p class="h4 Times-New-Roman">${recensione.user}</p>
+            <div class="d-flex justify-content-center star">
+
+            </div>
+        </div>
+        `;
+        swiperWrapper.appendChild(div);
+})
+
+let stars = document.querySelectorAll('.star');
+
+// <i class="fa-solid fa-star"></i>
+
+stars.forEach( (star, index)=>{
+
+    for (let i = 1; i <= reviews[index].rank; i++) {
+        let icon = document.createElement('i');
+        icon.classList.add('fa-solid', 'fa-star');
+        star.appendChild(icon);
+    }
+
+    let diffence = 5 - reviews[index].rank;
+
+    for (let i = 1; i <= diffence; i++) {
+        let icon = document.createElement('i');
+        icon.classList.add('fa-regular', 'fa-star');
+        star.appendChild(icon);
+    }
+
+});
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    loop: true,
+    
+    
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+
+
+
+
 
 
 
